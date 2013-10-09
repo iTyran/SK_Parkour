f08290a7d67d621cd2e1bc3bc35686bca923ce66
u0u0 <lkncastle@gmail.com>
2013-10-09 14:39:55 +0800

    change package name, remove "-" to make android build happy.

--- projects/Parkour/proj.android/src/org/cocos2d-x/parkour/Parkour.java
+++ projects/Parkour/proj.android/src/org/cocos2d-x/parkour/Parkour.java
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ****************************************************************************/
-package org.cocos2d-x.parkour;
+package org.cocos2dx.parkour;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 

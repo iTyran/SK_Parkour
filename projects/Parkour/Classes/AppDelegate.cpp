548c25ccacdca909c2d761ed71ddfc145c9354e0
u0u0 <lkncastle@gmail.com>
2013-10-09 14:11:08 +0800

    sync to 2.1.5 android project

--- projects/Parkour/Classes/AppDelegate.cpp
+++ projects/Parkour/Classes/AppDelegate.cpp
@@ -4,15 +4,16 @@
 #include "cocos2d.h"
 #include "SimpleAudioEngine.h"
 #include "ScriptingCore.h"
-#include "jsb_cocos2dx_auto.hpp"
-#include "jsb_cocos2dx_extension_auto.hpp"
+#include "generated/jsb_cocos2dx_auto.hpp"
+#include "generated/jsb_cocos2dx_extension_auto.hpp"
 #include "jsb_cocos2dx_extension_manual.h"
 #include "cocos2d_specifics.hpp"
 #include "js_bindings_chipmunk_registration.h"
-#include "js_bindings_ccbreader.h"
 #include "js_bindings_system_registration.h"
+#include "js_bindings_ccbreader.h"
 #include "jsb_opengl_registration.h"
 #include "XMLHTTPRequest.h"
+#include "jsb_websocket.h"
 
 USING_NS_CC;
 using namespace CocosDenshion;
@@ -23,7 +24,7 @@ AppDelegate::AppDelegate()
 
 AppDelegate::~AppDelegate()
 {
-    //    SimpleAudioEngine::end();
+    CCScriptEngineManager::purgeSharedManager();
 }
 
 bool AppDelegate::applicationDidFinishLaunching()
@@ -60,13 +61,14 @@ bool AppDelegate::applicationDidFinishLaunching()
     ScriptingCore* sc = ScriptingCore::getInstance();
     sc->addRegisterCallback(register_all_cocos2dx);
     sc->addRegisterCallback(register_all_cocos2dx_extension);
-    sc->addRegisterCallback(register_cocos2dx_js_extensions);
     sc->addRegisterCallback(register_all_cocos2dx_extension_manual);
+    sc->addRegisterCallback(register_cocos2dx_js_extensions);
     sc->addRegisterCallback(register_CCBuilderReader);
     sc->addRegisterCallback(jsb_register_chipmunk);
     sc->addRegisterCallback(jsb_register_system);
     sc->addRegisterCallback(JSB_register_opengl);
     sc->addRegisterCallback(MinXmlHttpRequest::_js_register);
+    sc->addRegisterCallback(register_jsb_websocket);
 
     sc->start();
     
@@ -75,6 +77,26 @@ bool AppDelegate::applicationDidFinishLaunching()
     ScriptingCore::getInstance()->runScript("MainScene.js");
     
     return true;
+}
+
+void handle_signal(int signal) {
+    static int internal_state = 0;
+    ScriptingCore* sc = ScriptingCore::getInstance();
+    // should start everything back
+    CCDirector* director = CCDirector::sharedDirector();
+    if (director->getRunningScene()) {
+        director->popToRootScene();
+    } else {
+        CCPoolManager::sharedPoolManager()->finalize();
+        if (internal_state == 0) {
+            //sc->dumpRoot(NULL, 0, NULL);
+            sc->start();
+            internal_state = 1;
+        } else {
+            sc->runScript("hello.js");
+            internal_state = 0;
+        }
+    }
 }
 
 // This function will be called when the app is inactive. When comes a phone call,it's be invoked too
